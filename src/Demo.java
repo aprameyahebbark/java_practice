c6d08f6db627aead80fc48434ba53ff6b14e725f
aprameyahebbark <aprameyahebbark@gmail.com>
2026-08-23 21:05:50 +0530

    Add Demo program

---
+++ src/Demo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Demo {
+    public static long reverse(long num) {
+        long rv = 0L;
+
+        while (num != 0) {
+            rv = (rv * 10) + (num % 10);
+            num /= 10;
+        }
+        return rv;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        if (input.hasNextLong()) {
+            long num = input.nextLong();
+            System.out.println(Demo.reverse(num));
+        }
+        input.close();
+    }
+}
