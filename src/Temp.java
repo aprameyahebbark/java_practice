f17f334f2c92ad9b4cc5761331546b67e68ad58c
aprameyahebbark <aprameyahebbark@gmail.com>
2026-08-23 21:06:10 +0530

    Add temperature conversion program

---
+++ src/Temp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Temp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Print the temp in Celcius");
+        float tempC = sc.nextFloat();
+        float tempF = ((tempC* 9/5) + 32);
+        System.out.println(tempF);
+    }
+}
