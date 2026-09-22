386b9ff8a22707b2fc915bab16469aee275661a6
aprameyahebbark <aprameyahebbark@gmail.com>
2026-09-23 00:56:55 +0530

    celcius to Fahrenheit

--- src/Temp.java
+++ src/Temp.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class Temp {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Print the temp in Celcius");
         float tempC = sc.nextFloat();
         float tempF = ((tempC* 9/5) + 32);
